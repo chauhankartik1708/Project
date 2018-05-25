64f82f11355db66ec75e411e5319ce1843f219a7
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-05-25 17:03:45 +0530

    finished with register and profile connecting with database
    Signed-off-by: Kartik Chauhan <lkchauhan.725@gmail.com>

--- facerecognitionbrain-api/server.js
+++ facerecognitionbrain-api/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '1708',
+        database: 'smart-brain'
+    }
+});
+
 
 const app = express();
 
@@ -44,30 +56,35 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req, res) => {
     const {email,password,name} = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
+    db('users')
+    .returning('*')
+    .insert({
         email: email,
-        entries: 0,
+        name: name,
         joined: new Date()
-    });
-
-    res.json(database.users[database.users.length -1]);
+    })
+    .then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('unable to register'))
+    
 })
 
 app.get('/profile/:id', (req,res)=> {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            return res.json(user);
+    
+    db.select('*').from('users').where({id})
+    .then(user => {
+        console.log(user)
+        if(user.length){
+           res.json(user[0]) 
+        }else{
+            res.status(400).json('not found')
         }
+        
     })
-
-    if(!found){
-        res.status(404).json('no such user');
-    }
+    .catch(err => res.status(400).json('error getting user'))
+    
 })
 
 app.put('/image', (req, res) => {
